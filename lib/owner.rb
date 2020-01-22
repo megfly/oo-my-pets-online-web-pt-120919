a01a483e2c7f5e24e6ddf2da3e20fd51763fac55
Meghan Flynn <mflynn333@gmail.com>
2020-01-22 01:12:34 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,31 @@
 class Owner
-  # code goes here
+  attr_accessor 
+  attr_reader :name, :species
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @species = "human"
+    @@all << self
+    @cats = []
+  end 
+  
+  def say_species
+    "I am a #{@species}."
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def self.count 
+    self.all.length
+  end 
+  
+  def self.reset_all 
+    self.all.clear
+  end 
+  
+ 
 end
